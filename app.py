3015a54601463a19699c81d902d5a8c9cfa355a0
hyunnn24 <snow3515@naver.com>
2024-06-18 07:21:43 +0000

    asgdfnhtghnftr

--- app.py
+++ app.py
@@ -2,5 +2,45 @@ import openai
 import streamlit
 from bs4 import BeautifulSoup
 import requests
+from openai import OpenAI
 
+api = st.text_input('Enter your OpenAI API key:', type='password')
 
+if api_key:
+    client = OpenAI(api_key=api)
+
+import time
+
+def run_and_wait(client, assistant, thread):
+  run = client.beta.threads.runs.create(
+    thread_id=thread.id,
+    assistant_id=assistant.id
+  )
+  while True:
+    run_check = client.beta.threads.runs.retrieve(
+      thread_id=thread.id,
+      run_id=run.id
+    )
+    print(run_check.status)
+    if run_check.status in ['queued','in_progress']:
+      time.sleep(2)
+    else:
+      break
+  return run
+
+def download_and_save(url, filename):
+  r = requests.get(url)
+  soup = BeautifulSoup(r.text, 'html.parser')
+  text = soup.get_text(separator=' ', strip=True)
+  with open(filename,'w') as fo:
+    fo.write(text)
+
+url = "https://github.com/hyunnn24/retrieval/blob/main/data.txt"
+filename = 'data.txt'
+
+download_and_save(url, filename)
+
+with open(filename) as fi:
+  text = fi.read()
+
+st.write(text)
