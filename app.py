650a8f8b06c7015f96a50f9529485f49f65c821b
hyunnn24 <snow3515@naver.com>
2024-06-19 07:11:38 +0000

    test

--- app.py
+++ app.py
@@ -9,24 +9,6 @@ api = st.text_input('Enter your OpenAI API key:', type='password')
 if api:
     client = OpenAI(api_key=api)
 
-import time
-
-def run_and_wait(client, assistant, thread):
-  run = client.beta.threads.runs.create(
-    thread_id=thread.id,
-    assistant_id=assistant.id
-  )
-  while True:
-    run_check = client.beta.threads.runs.stream(
-      thread_id=thread.id,
-      run_id=run.id
-    )
-    print(run_check.status)
-    if run_check.status in ['queued','in_progress']:
-      time.sleep(2)
-    else:
-      break
-  return run
 
 def download_and_save(url, filename):
   r = requests.get(url)
@@ -44,36 +26,12 @@ download_and_save(url, filename)
 with open(filename) as fi:
   text = fi.read()
 
-#st.write(text)
+st.write(text)
 if api:
-  my_file = client.files.create(
-      file = open(filename,'rb'),
-      purpose='assistants'
-  )
-
   assistant = client.beta.assistants.create(
-    name="Financial Analyst Assistant",
-    instructions="You're a LOL bottom pick expert. Look at the file and answer.",
+    name="LOL Pick Assistant",
+    instructions="You are an expert of league of legend bottonline pick.",
     model="gpt-4o",
     tools=[{"type": "file_search"}],
   )
 
-
-ask=st.text_input("상대픽을 입력하세요:")
-if ask:
-    thread = client.beta.threads.create(
-    messages=[
-            {
-            "role": "user",
-            "content": ask,
-            #"file_ids": [my_file.id]
-            }
-        ]
-    )
-    thread
-
-    run_and_wait(client, assistant, thread)
-if run_check.status in ['completed']:
-    thread_messages = client.beta.threads.messages.list(thread.id)
-    for msg in thread_messages.data:
-        st.write(f"{msg.role}: {msg.content[0].text.value}")
